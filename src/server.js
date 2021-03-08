55ac6a1112abded5bb8cd6ee9daa6bfc727deb39
ksolbrig <brillosaurus202@gmx.de>
2021-03-08 01:01:03 +0100

    Starten des Servers mit dem Befehl node server.js, Aufrufen der Seite mit localhost:3000/, Anpassen der Source der JavaScript-  und CSS-Dateien in SpielGUI.html, Auslesen von languages.txt mit localhost:3000/languages, Anzeigen der Datei Testsprache.json mit localhost:3000/testsprache

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 let express = require("express");
-let app = express();
 let path = require("path");
 
+let app = express();
 
+
+app.use(express.static(__dirname));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/SpielGUI.html'));
+    res.sendFile(path.join(__dirname, 'SpielGUI.html'));
 });
 
-app.get('/', function(req, res) {
-    // ToDo Auslesen des Textes aus languages.txt
-    res.send();
+app.get('/languages', function(req, res) {
+    let fs = require("fs");
+    fs.readFile(path.join(__dirname,  'data', 'languages.txt'), function (err, data){
+        if(err){
+            return console.error(err);
+        }
+        res.send(data.toString());
+    })
 });
 
+app.get('/testsprache', function(req, res){
+    res.sendFile(path.join(__dirname, 'data', 'Testsprache.json'));
+})
+
+app.listen(3000); // NodeJS-WebServer lauscht an Port 3000
+console.log("Running at port 3000");
